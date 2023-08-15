fff81a0ca9ffecce32a4e4f072b8fcdfe0da3f00
Step_atlas <stp.sip7@gmail.com>
2023-08-14 19:53:11 -0500

    mobile side-bar and fix sidebar bug

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,13 @@
-// Components
-import { Button } from "@/components/ui/button";
 // auth Components
 import { UserButton } from "@clerk/nextjs";
-//Icons
-import { Menu } from "lucide-react";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <div className="flex items-center p-4">
+        <MobileSidebar />
+      </div>
       <div className="flex w-full justify-end ">
         <UserButton afterSignOutUrl="/" />
       </div>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,6 +14,7 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -64,17 +65,21 @@ const routes = [
   },
 ];
 const Sidebar = () => {
+  const router = useRouter();
   return (
-    <div className="space-y-4 py-4 flex flex-col h-hull bg-[#111827] text-white">
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+        <button
+          onClick={() => router.push("/dashboard")}
+          className="flex items-center pl-3 mb-14"
+        >
           <div className="relative w-8 h-8 mr-4">
             <Image src="/logo.png" alt="Logo" fill />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
             Genius
           </h1>
-        </Link>
+        </button>
         <div className="space-y-1">
           {routes.map((route) => (
             <Link
